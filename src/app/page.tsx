6feb61c39fc85b8ca397a195ece8bbe8d8ac1c25
usman2789 <mangoapple027@gmail.com>
2025-09-10 23:15:24 +0500

    make footer ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import Header from "../components/section/header";
-import Footer from "../components/section/footer";
+
 
 export default function Home() {
   return (
    <div>
-    <Header />
+    
     <div style={{ minHeight: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
       <h1>This is al taj site</h1>
     </div>
-    <Footer/>
+   
    </div>
   );
 }
