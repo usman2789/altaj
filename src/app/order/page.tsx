15cbb57ed686ca60f0abf325b5d80e868147f06a
usman2789 <mangoapple027@gmail.com>
2025-10-04 18:33:03 +0500

    es lint

---
+++ src/app/order/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <div className="min-h-screen bg-[#040402] text-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-white mb-4">
+            Order <span className="text-[#D4A541]">Online</span>
+          </h1>
+          <p className="text-gray-300 text-lg">
+            Coming soon! You&apos;ll be able to order your favorite dishes online.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
