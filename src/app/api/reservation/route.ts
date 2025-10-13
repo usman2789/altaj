5f94f7e4d40164d5a342a4fb6004d21b81b92a0f
usman2789 <mangoapple027@gmail.com>
2025-10-13 21:12:08 +0500

    api

--- src/app/api/reservation/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-// API route handlers
-
-export async function POST(request: NextRequest) {
-    const body = await request.json()
-    return NextResponse.json({ data: body })
-}
